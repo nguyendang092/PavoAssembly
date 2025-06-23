f47424d7dfede058094da2710682846eff86d1a5
nguyendang092 <trungnguyencv092@gmail.com>
2025-06-23 16:50:32 +0700

    update

--- src/AreaProductionTable.jsx
+++ src/AreaProductionTable.jsx
@@ -427,7 +427,7 @@ const AreaProductionTable = ({ area, showToast }) => {
         <div className="flex items-center gap-2 mt-5">
           <input
             type="text"
-            placeholder="Thêm model mới"
+            placeholder="Thêm Line mới"
             value={newModelName}
             onChange={(e) => setNewModelName(e.target.value)}
             className="border border-green-300 px-3 py-2 flex-1 rounded-lg shadow-inner focus:outline-none focus:ring-2 focus:ring-green-400 transition bg-white"
